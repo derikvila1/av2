84b7af357019512755d4495e5959b1644c603baa
Derik Vila <derikvila@gmail.com>
2021-06-20 15:04:06 -0400

    first

---
+++ av2.ts
@@ -0,0 +1,24 @@
+ //SINGLETON
+ 
+ export class Database {
+   private static database: Database;
+
+    private constructor(
+        private host : string, 
+        private user : string, 
+        private password: string
+        ){}
+        connect(): void {
+            console.log(`conectado: ${this.host}, ${this.user}, ${this.password}}`);
+        }
+
+        static getDatabase(host : string, user : string, password: string): Database{
+            if (Database.database) return Database.database
+            console.log('retornando ja iniciado');
+            Database.database = new Database(host, user, password);
+            return Database.database;
+        }
+    }
+
+    const db1 = Database.getDatabase('localhost', 'root', '123456');
+    db1.connect();
