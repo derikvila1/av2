1cce7c3b332126cc132e681d8d110410fc244d1b
Derik Vila <derikvila@gmail.com>
2021-06-22 15:17:41 -0400

    2

--- av2.ts
+++ av2.ts
@@ -8,13 +8,17 @@
         private user : string, 
         private password: string
         ){}
-        connect(): void {
-            console.log(`conectado: ${this.host}, ${this.user}, ${this.password}}`);
-        }
 
+        connect(): void {
+            console.log(`conectado: ${this.host}, ${this.user}, ${this.password}} \n`);
+        }
         static getDatabase(host : string, user : string, password: string): Database{
-            if (Database.database) return Database.database
-            console.log('retornando ja iniciado');
+            if (Database.database) 
+            {
+                console.log('retornando ja iniciado'); 
+                return Database.database
+            }
+            console.log('criando nova instancia' );
             Database.database = new Database(host, user, password);
             return Database.database;
         }
@@ -22,3 +26,8 @@
 
     const db1 = Database.getDatabase('localhost', 'root', '123456');
     db1.connect();
+
+    const db2 = Database.getDatabase('localhost', 'root', '123456');
+    db2.connect();
+
+    console.log(db1 === db2);
